58f3c4b10bb4fca972de800047b43a172efa813a
ToMMaaY <thuynh629@gmail.com>
2019-05-18 21:14:00 -0700

    seeded data for types

---
+++ data/seeds/003-types.js
@@ -0,0 +1,14 @@
+exports.seed = function(knex, Promise) {
+    // Deletes ALL existing entries
+    return knex('types')
+        .truncate()
+        .then(function() {
+            // Inserts seed entries
+            return knex('types').insert([
+                { name: 'Hybrid' },
+                { name: 'Sativa' },
+                { name: 'Indica' },
+                { name: 'CBD' },
+            ]);
+        });
+};
