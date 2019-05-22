b920bc1a1b449bb2054592ab65651fce850276a0
ToMMaaY <thuynh629@gmail.com>
2019-05-22 15:13:54 -0700

    added endpoint to upload images

---
+++ api/apidoc/imageDoc.js
@@ -0,0 +1,8 @@
+/**
+ * @api {post} /api/products/upload Upload an image
+ * @apiSuccess {String} iamgeUrl Should return an image url
+ * @apiSuccessExample {json} Success Response:
+ *      200 OK
+ *
+ *      "http://res.cloudinary.com/ibetomm/image/upload/v1558562916/ywsfmpg4jxnfuq2dltgs.jpg"
+ */
--- api/routes/productRoutes.js
+++ api/routes/productRoutes.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const {
+    cloudinaryConfig,
+    uploader,
+} = require('../../middleware/cloudinaryConfig');
+const { multerUploads, dataUri } = require('../../middleware/multerConfig');
+
+// cloudinary configuration for the router
+cloudinaryConfig(router);
 
 // productsModel contains the functions to get data from the products table
 const productsModel = require('../model/productsModel');
@@ -86,6 +94,28 @@ router.patch('/:id', async (req, res, next) => {
                   .json({ error: `There is no product with the id: ${id}` });
     } catch (err) {
         next(err);
+    }
+});
+
+// uploads image to Cloudinary and returns an image url
+router.post('/upload', multerUploads, (req, res) => {
+    if (req.file) {
+        const file = dataUri(req).content;
+
+        return uploader.upload(file).then(result => {
+            const image = result.url;
+
+            return res
+                .status(200)
+                .json(image)
+                .catch(err =>
+                    res.status(400).json({
+                        message:
+                            'Something went wrong while processing your request',
+                        err: err,
+                    })
+                );
+        });
     }
 });
 
