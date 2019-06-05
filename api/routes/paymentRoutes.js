81de93563f95db281fe7a4ec76899f2b6665a6bf
ToMMaaY <thuynh629@gmail.com>
2019-06-04 19:17:06 -0700

    hooked up stripe api

--- api/routes/paymentRoutes.js
+++ api/routes/paymentRoutes.js
@@ -3,23 +3,14 @@ const router = express.Router();
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 router.post('/charge', async (req, res, next) => {
-    const { name, amount, quantity, stripeEmail, stripeToken, id } = req.body;
-
-    const info = await stripe.customers.create({
-        email: stripeEmail,
-        source: stripeToken,
-    });
-
-    const charge = await stripe.charges.create({
-        amount,
-        currency: 'usd',
-        description: 'Purchase from Flower Co.',
-        source: token,
-    });
-
-    let record = await db('orders').insert(record);
-
-    res.status(201).json(record);
+    console.log('req.body', req.body);
+    try {
+        const response = await stripe.charges.create(req.body);
+        console.log('res', res);
+        res.status(200).json(response);
+    } catch (err) {
+        next(err);
+    }
 });
 
 module.exports = router;
