b44d8bac67897d7c7ddece68b0ea7f915a8423f2
ToMMaaY <thuynh629@gmail.com>
2019-06-04 19:30:02 -0700

    reset the dev db

--- api/routes/paymentRoutes.js
+++ api/routes/paymentRoutes.js
@@ -3,10 +3,9 @@ const router = express.Router();
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 router.post('/charge', async (req, res, next) => {
-    console.log('req.body', req.body);
     try {
         const response = await stripe.charges.create(req.body);
-        console.log('res', res);
+
         res.status(200).json(response);
     } catch (err) {
         next(err);
